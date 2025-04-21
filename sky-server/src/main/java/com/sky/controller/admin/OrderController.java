b34f1e13560cace616211124a95a414be7df99d8
Ace <3506647717@qq.com>
2025-04-21 20:22:27 +0800

    管理端订单搜索功能

---
+++ sky-server/src/main/java/com/sky/controller/admin/OrderController.java
@@ -0,0 +1,37 @@
+package com.sky.controller.admin;
+
+import com.sky.dto.OrdersPageQueryDTO;
+import com.sky.result.PageResult;
+import com.sky.result.Result;
+import com.sky.service.OrderService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 管理端订单接口
+ */
+@RestController("adminOrderController")
+@RequestMapping("/admin/order")
+@Slf4j
+public class OrderController {
+
+    @Autowired
+    private OrderService OrderService;
+
+
+    /**
+     * 订单搜索
+     * @param ordersPageQueryDTO
+     * @return
+     */
+    @GetMapping("/conditionSearch")
+    public Result<PageResult> conditionSearch(OrdersPageQueryDTO ordersPageQueryDTO) {
+        log.info("条件查询订单，参数：{}", ordersPageQueryDTO);
+        PageResult pageResult = OrderService.conditionSearch(ordersPageQueryDTO);
+        return Result.success(pageResult);
+    }
+
+}
